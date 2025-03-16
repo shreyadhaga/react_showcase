c2c4fb2f1c82cacd828ee4fc21489e8c0d101bdb
shreyadhaga <shreyadhaga@gmail.com>
2025-03-16 15:44:19 +0530

    Reusable Component

--- Fundamentals/src/App.jsx
+++ Fundamentals/src/App.jsx
@@ -1,4 +1,8 @@
-import CoreConceptsImg from "./assets/react-core-concepts.png";
+import mainAtomImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
+import configImg from "./assets/config.png";
+import stateManagementImg from "./assets/state-mgmt.png";
+import jsxImg from "./assets/jsx-ui.png";
 
 const reactDescriptions = ["Fundamental", "Crutial", "Core"];
 
@@ -10,7 +14,7 @@ function Header() {
   const description = reactDescriptions[genRandomInt(2)];
   return (
     <header>
-      <img src={CoreConceptsImg} alt="Stylized atom" />
+      <img src={mainAtomImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
@@ -20,12 +24,46 @@ function Header() {
   );
 }
 
+function CoreConcept({ title, description, image }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The core UI building block."
+              image={componentsImg}
+            />
+            <CoreConcept
+              title="Components"
+              description="The core UI building block."
+              image={configImg}
+            />
+            <CoreConcept
+              title="Components"
+              description="The core UI building block."
+              image={stateManagementImg}
+            />
+            <CoreConcept
+              title="Components"
+              description="The core UI building block."
+              image={jsxImg}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );

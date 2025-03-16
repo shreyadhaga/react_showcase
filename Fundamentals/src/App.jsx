86c602fe0c1c656af25b4a51d00e3b89f766031e
shreyadhaga <shreyadhaga@gmail.com>
2025-03-16 22:36:24 +0530

    conditional rendering

--- Fundamentals/src/App.jsx
+++ Fundamentals/src/App.jsx
@@ -1,8 +1,16 @@
-import { CORE_CONCEPTS } from "./data";
+import { useState } from "react";
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept/CoreConcept";
+import TabButton from "./components/TabButton/TabButton";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState("jsx");
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
   return (
     <div>
       <Header />
@@ -14,6 +22,28 @@ function App() {
               <CoreConcept {...item} />
             ))}
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}> JSX </TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
+          {!selectedTopic ? (
+            <p>Please select topic</p>
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
         </section>
       </main>
     </div>

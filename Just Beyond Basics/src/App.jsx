0fd36ac517f687007485c99d905abee816e15da2
dhaga-shreya <dhaga_shreya@network.lilly.com>
2025-03-17 16:38:56 +0530

    Fragments do not create extra tags on rendering

--- Just Beyond Basics/src/App.jsx
+++ Just Beyond Basics/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, Fragment } from "react";
 import { CORE_CONCEPTS, EXAMPLES } from "./data";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept/CoreConcept";
@@ -25,7 +25,7 @@ function App() {
     );
   }
   return (
-    <div>
+    <Fragment>
       <Header />
       <main>
         <section id="core-concepts">
@@ -49,7 +49,7 @@ function App() {
           {tabContent}
         </section>
       </main>
-    </div>
+    </Fragment>
   );
 }
 

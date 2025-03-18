23817a110c71c79b1cb778b68b1e7dcc85bae6e8
shreyadhaga <shreyadhaga@gmail.com>
2025-03-19 00:26:02 +0530

    Basic Player Name

--- Tic-Tac-Toe/src/components/Player/Player.jsx
+++ Tic-Tac-Toe/src/components/Player/Player.jsx
@@ -1,11 +1,36 @@
-export default function Player({ name, symbol }) {
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    // best practice when updating from previous state is to use a function
+    // since react batches its updates
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  let buttonCaption = "Edit";
+  if (isEditing) {
+    // Two way binding is when the on change sets the player name and then reflects them in the value attribute
+    editablePlayerName = (
+      <input type="text" value={playerName} required onChange={handleChange} />
+    );
+    buttonCaption = "Save";
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button> Edit </button>
+      <button onClick={handleEditClick}>{buttonCaption}</button>
     </li>
   );
 }

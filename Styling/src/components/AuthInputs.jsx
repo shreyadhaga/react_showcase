f2e84064a005b3f38358b99df781fe26644ad9f7
shreyadhaga <shreyadhaga@gmail.com>
2025-04-12 10:33:30 +0530

    introduction to taiwind

--- Styling/src/components/AuthInputs.jsx
+++ Styling/src/components/AuthInputs.jsx
@@ -1,14 +1,6 @@
 import { useState } from "react";
-import { styled } from "styled-components";
 import Button from "./Button";
 import Input from "./Input";
-// tagged template
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -31,8 +23,11 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div
+      id="auth-inputs"
+      className="w-full max-w-sm mx-auto p-8 rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800"
+    >
+      <div className="flex flex-col gap-2 mb-6 ">
         <p className="paragraph">
           <Input
             invalid={emailNotValid}
@@ -51,12 +46,12 @@ export default function AuthInputs() {
             }
           />
         </p>
-      </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
-        <Button className="button" onClick={handleLogin}>
+        <Button onClick={handleLogin}>
           Sign In
         </Button>
       </div>
